1d31f7cec966138e3bdbd1279e40f78c31ca93ab
Toby Jackson <toby@warmfusion.co.uk>
2017-03-26 21:02:09 +0100

    oo pretty

--- main.go
+++ main.go
@@ -12,12 +12,10 @@ import (
 )
 
 var (
-	rows         = flag.Int("led-rows", 32, "number of rows supported")
-	parallel     = flag.Int("led-parallel", 1, "number of daisy-chained panels")
-	chain        = flag.Int("led-chain", 2, "number of displays daisy-chained")
-	brightness   = flag.Int("brightness", 100, "brightness (0-100)")
-	steps        = flag.Int("steps", 20, "brightness (0-100)")
-	frameCounter = 0
+	rows       = flag.Int("led-rows", 32, "number of rows supported")
+	parallel   = flag.Int("led-parallel", 1, "number of daisy-chained panels")
+	chain      = flag.Int("led-chain", 2, "number of displays daisy-chained")
+	brightness = flag.Int("brightness", 100, "brightness (0-100)")
 )
 
 func main() {
@@ -51,6 +49,7 @@ type Animation struct {
 	ctx      *gg.Context
 	position image.Point
 	dir      image.Point
+	steps    int
 	stroke   int
 }
 
@@ -58,6 +57,7 @@ func NewAnimation(sz image.Point) *Animation {
 	return &Animation{
 		ctx:    gg.NewContext(sz.X, sz.Y),
 		dir:    image.Point{1, 1},
+		steps:  0,
 		stroke: 10,
 	}
 }
@@ -67,16 +67,19 @@ func (a *Animation) Next() (image.Image, <-chan time.Time, error) {
 	a.ctx.SetColor(color.Black)
 	a.ctx.Clear()
 
-	a.ctx.DrawCircle(30.0, 10.0, float64(a.stroke))
-	a.ctx.SetColor(colorful.Hsv(216.0, 0.56, 0.3))
+	a.steps += 20
+	c := a.steps % 360
+
+	a.ctx.DrawCircle(30, 10.0, float64(a.stroke))
+	a.ctx.SetColor(colorful.Hsv(float64(c), 0.56, 0.3))
 	a.ctx.Fill()
 
-	a.ctx.DrawCircle(40.0, 10.0, float64(a.stroke))
-	a.ctx.SetColor(colorful.Hsv(90.0, 0.56, 0.3))
+	a.ctx.DrawCircle(35, 15.0, float64(a.stroke))
+	a.ctx.SetColor(colorful.Hsv(float64((c+90)%360), 0.56, 0.3))
 	a.ctx.Fill()
 
-	a.ctx.DrawCircle(30.0, 20.0, float64(a.stroke))
-	a.ctx.SetColor(colorful.Hsv(170.0, 0.56, 0.3))
+	a.ctx.DrawCircle(40, 20.0, float64(a.stroke))
+	a.ctx.SetColor(colorful.Hsv(float64((c+180)%360), 0.56, 0.3))
 	a.ctx.Fill()
 
 	return a.ctx.Image(), time.After(time.Millisecond * 50), nil
