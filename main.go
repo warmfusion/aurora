b3603f933e6f1dceec7625bb1db53454de9e3dd9
Toby Jackson <toby@warmfusion.co.uk>
2017-03-26 22:38:05 +0100

    Trying to draw a pretty curve... turns out - kinda hard

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"image"
 	"image/color"
+	"math"
 	"time"
 
 	"github.com/fogleman/gg"
@@ -58,7 +59,7 @@ func NewAnimation(sz image.Point) *Animation {
 		ctx:          gg.NewContext(sz.X, sz.Y),
 		dir:          image.Point{1, 1},
 		frameCounter: 0,
-		stroke:       10,
+		stroke:       6,
 	}
 }
 
@@ -67,11 +68,15 @@ func (a *Animation) Next() (image.Image, <-chan time.Time, error) {
 	a.ctx.SetColor(color.Black)
 	a.ctx.Clear()
 
-	a.frameCounter += 20
+	a.frameCounter++
 	c := a.frameCounter % 360
 
-	a.ctx.DrawCircle(30, 10.0, float64(a.stroke))
+	a.ctx.DrawCircle(25, 5.0, float64(a.stroke))
 	a.ctx.SetColor(colorful.Hsv(float64(c), 0.56, 0.3))
+	a.ctx.Fill()
+
+	a.ctx.DrawCircle(30, 10.0, float64(a.stroke))
+	a.ctx.SetColor(colorful.Hsv(float64(c+45), 0.56, 0.3))
 	a.ctx.Fill()
 
 	a.ctx.DrawCircle(35, 15.0, float64(a.stroke))
@@ -79,8 +84,36 @@ func (a *Animation) Next() (image.Image, <-chan time.Time, error) {
 	a.ctx.Fill()
 
 	a.ctx.DrawCircle(40, 20.0, float64(a.stroke))
+	a.ctx.SetColor(colorful.Hsv(float64((c+135)%360), 0.56, 0.3))
+	a.ctx.Fill()
+
+	a.ctx.DrawCircle(45, 25.0, float64(a.stroke))
 	a.ctx.SetColor(colorful.Hsv(float64((c+180)%360), 0.56, 0.3))
 	a.ctx.Fill()
+
+	// Lets try some animation...
+
+	//	angle := float64(a.frameCounter % 360)
+	angle := float64(0)
+	y := float64(0)
+	for x := 0; x < a.ctx.Width(); x++ {
+		/* calculate y value given x */
+		y = float64(50 * math.Sin(angle*3.141/180))
+		y = float64(a.ctx.Height()/2) - y
+
+		/* color a pixel at the given position */
+		a.ctx.DrawCircle(float64(x), float64((y/10)+16), 2.0)
+		a.ctx.SetColor(colorful.Hsv(0, 0.56, 0.0))
+		a.ctx.Fill()
+
+		a.ctx.DrawCircle(float64(x), float64((y/10)+16), 1.0)
+		a.ctx.SetColor(colorful.Hsv(float64((c+x)%360), 0.56, 0.5))
+		a.ctx.Fill()
+
+		/* increment angle */
+		angle += 5
+		angle = math.Mod(360, angle)
+	}
 
 	return a.ctx.Image(), time.After(time.Millisecond * 50), nil
 }
