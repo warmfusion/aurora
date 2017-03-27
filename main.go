ed443e2e13de780a1a769eb35e4fb96ba3292454
Toby Jackson <toby@warmfusion.co.uk>
2017-03-27 23:06:44 +0100

    Getting there...

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"flag"
 	"image"
 	"image/color"
-	"math"
+	"math/rand"
 	"time"
 
 	"github.com/fogleman/gg"
@@ -17,7 +17,21 @@ var (
 	parallel   = flag.Int("led-parallel", 1, "number of daisy-chained panels")
 	chain      = flag.Int("led-chain", 2, "number of displays daisy-chained")
 	brightness = flag.Int("brightness", 100, "brightness (0-100)")
+	maxSparks  = flag.Int("max-sparks", 100, "sparks (0-100)")
+	sparks     = make([]Spark, *maxSparks) // the slice v now refers to a new array of 100 ints
 )
+
+type Spark struct {
+	radius    float64
+	color     color.Color
+	position  SparkPoint
+	direction SparkPoint
+}
+
+type SparkPoint struct {
+	X float64
+	Y float64
+}
 
 func main() {
 	config := &rgbmatrix.DefaultConfig
@@ -34,6 +48,7 @@ func main() {
 	defer tk.Close()
 
 	tk.PlayAnimation(NewAnimation(image.Point{64, 32}))
+
 }
 
 func init() {
@@ -55,6 +70,14 @@ type Animation struct {
 }
 
 func NewAnimation(sz image.Point) *Animation {
+	for i := range sparks {
+		sparks[i] = Spark{
+			position:  SparkPoint{32, 16},
+			radius:    3.0,
+			color:     colorful.Hsv(rand.Float64()*360, 0.56, 0.3),
+			direction: SparkPoint{(rand.Float64() * 2) - 1, (rand.Float64() * 2) - 1},
+		}
+	}
 	return &Animation{
 		ctx:          gg.NewContext(sz.X, sz.Y),
 		dir:          image.Point{1, 1},
@@ -65,51 +88,45 @@ func NewAnimation(sz image.Point) *Animation {
 
 func (a *Animation) Next() (image.Image, <-chan time.Time, error) {
 
-	a.ctx.SetColor(color.Black)
-	a.ctx.Clear()
-
 	a.frameCounter++
 	c := a.frameCounter % 360
 
-	a.ctx.DrawCircle(25, 5.0, float64(a.stroke))
-	a.ctx.SetColor(colorful.Hsv(float64(c), 0.56, 0.3))
-	a.ctx.Fill()
+	// Restart
+	a.ctx.SetColor(color.Black)
+	a.ctx.Clear()
 
-	a.ctx.DrawCircle(30, 10.0, float64(a.stroke))
-	a.ctx.SetColor(colorful.Hsv(float64(c+45), 0.56, 0.3))
-	a.ctx.Fill()
+	for s := range sparks {
 
-	a.ctx.DrawCircle(35, 15.0, float64(a.stroke))
-	a.ctx.SetColor(colorful.Hsv(float64((c+90)%360), 0.56, 0.3))
-	a.ctx.Fill()
+		spark := &sparks[s]
 
-	a.ctx.DrawCircle(40, 20.0, float64(a.stroke))
-	a.ctx.SetColor(colorful.Hsv(float64((c+135)%360), 0.56, 0.3))
-	a.ctx.Fill()
+		// Linear movement for now
+		spark.position.X += spark.direction.X
+		spark.position.Y += spark.direction.Y
 
-	a.ctx.DrawCircle(45, 25.0, float64(a.stroke))
-	a.ctx.SetColor(colorful.Hsv(float64((c+180)%360), 0.56, 0.3))
-	a.ctx.Fill()
-
-	// Lets try some animation...
-
-	//	angle := float64(a.frameCounter % 360)
-	angle := float64(0)
-	y := float64(0)
-	for x := 0; x < a.ctx.Width(); x++ {
-		/* calculate y value given x */
-		y = float64(50 * math.Sin(angle*3.141/180))
-		y = float64(a.ctx.Height()/2) - y
-
-		/* color a pixel at the given position */
-		a.ctx.DrawCircle(float64(x), float64((y/10)+16), 1.0)
-		a.ctx.SetColor(colorful.Hsv(float64((c+x)%360), 0.56, 0.5))
+		a.ctx.SetColor(spark.color)
+		a.ctx.DrawCircle(spark.position.X, spark.position.Y, float64(spark.radius))
+		a.ctx.Fill()
+	}
+	/*
+		a.ctx.SetColor(colorful.Hsv(float64(c), 0.56, 0.3))
+		a.ctx.DrawCircle(25, 5.0, float64(a.stroke))
 		a.ctx.Fill()
 
-		/* increment angle */
-		angle += 5
-		angle = math.Mod(360, angle)
-	}
+		a.ctx.SetColor(colorful.Hsv(float64(c+45), 0.56, 0.3))
+		a.ctx.DrawCircle(30, 10.0, float64(a.stroke))
+		a.ctx.Fill()
 
+		a.ctx.SetColor(colorful.Hsv(float64((c+90)%360), 0.56, 0.3))
+		a.ctx.DrawCircle(35, 15.0, float64(a.stroke))
+		a.ctx.Fill()
+
+		a.ctx.SetColor(colorful.Hsv(float64((c+135)%360), 0.56, 0.3))
+		a.ctx.DrawCircle(40, 20.0, float64(a.stroke))
+		a.ctx.Fill()
+
+		a.ctx.SetColor(colorful.Hsv(float64((c+180)%360), 0.56, 0.3))
+		a.ctx.DrawCircle(45, 25.0, float64(a.stroke))
+		a.ctx.Fill()
+	*/
 	return a.ctx.Image(), time.After(time.Millisecond * 50), nil
 }
